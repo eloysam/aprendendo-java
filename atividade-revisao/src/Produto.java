7eac3a3c8f5240dd057ace529f2d5024458b6528
Eloysa Ferreira Monteiro <114229411+eloysam@users.noreply.github.com>
2024-04-20 10:11:44 -0300

    Update Produto.java

--- atividade-revisao/src/Produto.java
+++ atividade-revisao/src/Produto.java
@@ -36,35 +36,48 @@ public class Produto {
 /******************************** HASHCODE E EQUALS ************************************************************************************** */
     @Override
     public int hashCode() {
-
         final int prime = 31;
         int result = 1;
         result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((qtde == null) ? 0 : qtde.hashCode());
+        result = prime * result + ((valor == null) ? 0 : valor.hashCode());
         return result;
     }
-
     @Override
     public boolean equals(Object obj) {
-
         if (this == obj)
             return true;
-
         if (obj == null)
             return false;
-
         if (getClass() != obj.getClass())
             return false;
-
         Produto other = (Produto) obj;
-
         if (id == null) {
             if (other.id != null)
                 return false;
-                
         } else if (!id.equals(other.id))
+            return false;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (qtde == null) {
+            if (other.qtde != null)
+                return false;
+        } else if (!qtde.equals(other.qtde))
+            return false;
+        if (valor == null) {
+            if (other.valor != null)
+                return false;
+        } else if (!valor.equals(other.valor))
             return false;
         return true;
     }
+
+
+
 
     /* Basicamente o equals faz uma comparação entre a referência de dois objetos */ 
 
