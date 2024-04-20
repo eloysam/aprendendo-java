6cd3b253e06fcf529e4aa8d5c27335a6de304549
Eloysa Ferreira Monteiro <114229411+eloysam@users.noreply.github.com>
2024-04-20 09:54:05 -0300

    criando Produto.java

---
+++ atividade-revisao/src/Produto.java
@@ -0,0 +1,33 @@
+public class Produto {
+    private Long id;
+    private String nome;
+    private Integer qtde;
+    private Double valor;
+
+    public Long getId() {
+        return this.id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return this.nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Integer getQtde() {
+        return this.qtde;
+    }
+    public void setQtde(Integer qtde) {
+        this.qtde = qtde;
+    }
+    public Double getValor() {
+        return this.valor;
+    }
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    
+}
