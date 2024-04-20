02478dde62ad54204d886329b439a127f31afd9e
Eloysa Ferreira Monteiro <114229411+eloysam@users.noreply.github.com>
2024-04-20 10:10:16 -0300

    Update Produto.java

--- atividade-revisao/src/Produto.java
+++ atividade-revisao/src/Produto.java
@@ -33,6 +33,41 @@ public class Produto {
         return "Id:" + this.id + "\nNome:" + this.nome + "\nQtde:" + this.qtde + "\nValor:" + this.valor;
     }
 
+/******************************** HASHCODE E EQUALS ************************************************************************************** */
+    @Override
+    public int hashCode() {
+
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj)
+            return true;
+
+        if (obj == null)
+            return false;
+
+        if (getClass() != obj.getClass())
+            return false;
+
+        Produto other = (Produto) obj;
+
+        if (id == null) {
+            if (other.id != null)
+                return false;
+                
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    /* Basicamente o equals faz uma comparação entre a referência de dois objetos */ 
+
     
     
 }
